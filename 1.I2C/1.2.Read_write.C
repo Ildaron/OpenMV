32ea0c05fa99db2500de4234b18e83a2505cd5e2
Ildar <de.ikar@yandex.ru>
2020-12-27 00:44:35 +0900

    Update 1.2.Read_write.C

--- 1.I2C/1.2.Read_write.C
+++ 1.I2C/1.2.Read_write.C
@@ -95,11 +95,7 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
+ 
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
